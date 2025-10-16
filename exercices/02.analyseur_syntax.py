28609af84bfc78cfb830eb62d505d36d6f839fd7
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2025-10-16 12:15:03 +0200

    update

--- exercices/02.analyseur_syntax.py
+++ exercices/02.analyseur_syntax.py
@@ -5,11 +5,22 @@
     # (() : False
     # )( : False
 
-def analyseur_syntaxique(x):
-    print(x)
+def analyseur_syntaxique(chaine):
+    stack = []
+    print(f"Analyse de: {chaine}")
+    for car in chaine:
+        if car == "(":
+            stack.append(car)
+        elif car == ")":
+            if not stack:
+                print(False)
+                return
+            stack.pop()
+    print(len(stack) == 0)
 
 analyseur_syntaxique("()")
 analyseur_syntaxique("()()")
 analyseur_syntaxique("(())")
 analyseur_syntaxique("(()")
 analyseur_syntaxique(")(")
+analyseur_syntaxique("(()))")
