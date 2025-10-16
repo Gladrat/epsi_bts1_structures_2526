3cc9a11bb95d6b24251d1b9daccfc93230f2e94b
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2025-10-16 11:04:22 +0200

    update

---
+++ cours/03. files_piles.py
@@ -0,0 +1,31 @@
+# PILES / STACK
+    # LIFO
+
+# Algorithmie : ADP
+
+    # Méthodes classiques
+        # push() : ajoute sur la pile
+        # pop() : retire ET renvoi la dernière valeur
+        # peek() : lire la dernière valeur
+
+# Implémentation
+
+    # C++::stack
+    # Python : list (append, pop et -1)
+
+# Analyseur syntaxique
+
+print(len([1, 2, 3, len([4, 5, 6]), [7, 8, 9].pop()]))
+
+()
+()()
+[(([{[]()}, {()}]))([])]
+
+# Vous devez construire un analyseur syntaxique
+    # () : True
+    # ()() : True
+    # (()) : True
+    # (() : False
+    # )( : False
+
+    
--- exercices/01.temperatures.py
+++ exercices/01.temperatures.py
@@ -1,25 +1,38 @@
 temperatures = [
-    [15, 14, 14, 13, 13, 14, 15, 16, 18, 20, 22, 23, 23, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14],
-    [14, 13, 13, 12, 12, 13, 14, 16, 18, 21, 23, 24, 25, 25, 24, 23, 22, 21, 19, 18, 17, 15, 14, 13],
-    [13, 12, 12, 11, 11, 12, 13, 15, 17, 20, 22, 23, 23, 24, 23, 22, 21, 20, 18, 17, 16, 14, 13, 12],
-    [14, 13, 13, 12, 12, 14, 15, 17, 19, 22, 24, 25, 26, 26, 25, 24, 23, 22, 20, 19, 18, 16, 15, 14],
-    [15, 14, 14, 13, 13, 15, 16, 18, 20, 23, 25, 26, 27, 27, 26, 25, 24, 23, 21, 20, 19, 17, 16, 15],
-    [16, 15, 15, 14, 14, 16, 17, 19, 21, 24, 26, 27, 28, 28, 27, 26, 25, 24, 22, 21, 20, 18, 17, 16],
-    [15, 14, 14, 13, 12, 14, 15, 17, 19, 22, 24, 25, 26, 26, 25, 24, 23, 22, 21, 20, 19, 17, 16, 15]
+    [15, 14, 14, 13, 13, 14, 15, 16, 18, 20, 22, 23,
+        23, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14],
+    [14, 13, 13, 12, 12, 13, 14, 16, 18, 21, 23, 24,
+        25, 25, 24, 23, 22, 21, 19, 18, 17, 15, 14, 13],
+    [13, 12, 12, 11, 11, 12, 13, 15, 17, 20, 22, 23,
+        23, 24, 23, 22, 21, 20, 18, 17, 16, 14, 13, 12],
+    [14, 13, 13, 12, 12, 14, 15, 17, 19, 22, 24, 25,
+        26, 26, 25, 24, 23, 22, 20, 19, 18, 16, 15, 14],
+    [15, 14, 14, 13, 13, 15, 16, 18, 20, 23, 25, 26,
+        27, 27, 26, 25, 24, 23, 21, 20, 19, 17, 16, 15],
+    [16, 15, 15, 14, 14, 16, 17, 19, 21, 24, 26, 27,
+        28, 28, 27, 26, 25, 24, 22, 21, 20, 18, 17, 16],
+    [15, 14, 14, 13, 12, 14, 15, 17, 19, 22, 24, 25,
+        26, 26, 25, 24, 23, 22, 21, 20, 19, 17, 16, 15]
 ]
 
 # Afficher PROPREMENT :
-    # Vérifier qu'il y a bien 24 températures pour chaque jour
-    # Quelle température à midi le mercredi ?
-    # Afficher la moyenne des températures sur un jour (idéalement dans une fonction)
-    # Afficher toutes les températures
+# Vérifier qu'il y a bien 24 températures pour chaque jour
+# Quelle température à midi le mercredi ?
+# Afficher la moyenne des températures sur un jour (idéalement dans une fonction)
+# Afficher toutes les températures
 
-j = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+jours = ["Lundi", "Mardi", "Mercredi",
+         "Jeudi", "Vendredi", "Samedi", "Dimanche"]
 
 for i, jour in enumerate(temperatures, 0):
-    print(f"Le {j[i]}, il y a {len(jour)} températures.")
+    print(f"Le {jours[i]}, il y a {len(jour)} températures.")
 
-i = j.index("Mercredi")
+i = jours.index("Mercredi")
 h = 12
-print(f"Le {j[2]} à midi, il fait: {temperatures[i][h + 1]}°")
+print(f"Le {jours[2]} à midi, il fait: {temperatures[i][h + 1]}°")
 
+for i, jour in enumerate(jours):
+    print(f"Température de {jour}:")
+    for j, heure in enumerate(temperatures[i]):
+        print(f"{j:02d}h: \033[94m{heure}°C \033[0m", end=" . ")
+    print("\n")
