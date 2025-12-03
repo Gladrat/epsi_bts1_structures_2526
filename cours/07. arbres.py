eeb47bffdae7a761bb784c297d319cb67541424a
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2025-12-03 15:04:13 +0100

    update

--- cours/07. arbres.py
+++ cours/07. arbres.py
@@ -16,18 +16,16 @@ from rich.pretty import pprint
 
 # 30, 50, 10, 20, 60
 
-#       30
-#     / |
-#    50  10
-#      / \
-#     20   60
+#         30
+#       /    \
+#     10     50
+#   /   \   /  \
+#      20      60
 
 # Arbres : Racine / Noeuds
 # Arbres binaires : Racine / Noeuds (2 max)
 # Binary Search Tree (BST) : Racine / Noeuds (2 max) / sous-arbre de gauche < racine & sous-arbre de droite > racine
 
-# creer_noeud
-# inserer
 
 def creer_noeud(val):
     return {"val": val, "g": None, "d": None}
@@ -36,7 +34,7 @@ def creer_noeud(val):
 def inserer(val, racine=None):
     if racine is None:
         return creer_noeud(val)
-    
+
     if val < racine["val"]:
         racine["g"] = inserer(val, racine["g"])
     else:
@@ -45,9 +43,26 @@ def inserer(val, racine=None):
     return racine
 
 
+def parcours_ordre(racine):
+    if racine is None:
+        return
+
+    parcours_ordre(racine["g"])
+    print(str(racine["val"]) + " ", end="")
+    parcours_ordre(racine["d"])
+
 # 50, 30, 70, 20, 40, 60, 80
 
-racine = inserer(30)
+
+# racine = inserer(30)
+racine = None
+values = [50, 30, 70, 20, 40, 60, 80]
+for v in values:
+    racine = inserer(v, racine)
 
 
 pprint(racine)
+parcours_ordre(racine)
+
+
+values.sort()
---
+++ exercices/sleep_sort.py
@@ -0,0 +1,15 @@
+import asyncio
+
+
+async def sort_val(val):
+    await asyncio.sleep(val)
+    print(val, end=" ", flush=True)
+
+
+async def sleep_sort(values):
+    tasks = [sort_val(v) for v in values]
+    await asyncio.gather(*tasks)
+
+
+n = [100000, 1]
+asyncio.run(sleep_sort(n))
